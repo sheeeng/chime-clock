6e4ed493f746c9337b521bc2b3d986ae984df9fe
Leonard Sheng Sheng Lee <305414+sheeeng@users.noreply.github.com>
2026-08-14 20:31:34 +0200

    build(nix): add google cloud sdk
    Signed-off-by: Leonard Sheng Sheng Lee <305414+sheeeng@users.noreply.github.com>

--- flake.nix
+++ flake.nix
@@ -23,6 +23,7 @@
         {
           default = pkgs.mkShell {
             packages = with pkgs; [
+              google-cloud-sdk # https://search.nixos.org/packages?channel=unstable&type=packages&show=google-cloud-sdk
               nodejs-slim # https://search.nixos.org/packages?channel=unstable&type=packages&show=nodejs-slim
             ];
 
